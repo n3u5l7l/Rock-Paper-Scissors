43b62d78cbaa35e55960b7f24135aa088e1d09d6
MingMing <greeteaz567@gmail.com>
2022-01-04 23:12:45 -0600

    Finished and tested single round Play function

--- script.js
+++ script.js
@@ -10,4 +10,18 @@ function computerPlay()
 
 }
 
-console.log(computerPlay());
+function play(computer, player)
+{
+    player = player.toLowerCase();
+    console.log(player);
+    console.log(computer);
+    if (computer.toLowerCase() === player){return "Tie!";}
+    else if (computer === "Rock" && player === "paper"){return "Player Wins!";}
+    else if (computer === "Paper" && player === "scissors"){return "Player Wins!";}
+    else if (computer === "Scissors" && player === "rock"){return "Player Wins!";}
+    else{return "Computer Wins!";}
+}
+
+const playerSelection = "rock";
+const computerSelection = computerPlay();
+console.log(play(computerSelection, playerSelection));
