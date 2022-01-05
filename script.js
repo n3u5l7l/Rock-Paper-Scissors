97ce725ef180c5cdaf6a3082f00c33c7ac37e9da
MingMing <greeteaz567@gmail.com>
2022-01-04 23:41:58 -0600

    Added a check if Player click cancel

--- script.js
+++ script.js
@@ -24,10 +24,12 @@ function game()
 {
     let playerWin = 0;
     let computerWin = 0;
+    let cancelCheck = false;
     for (let i = 0; i < 5; i++)
     {
         const computerChoice = computerPlay();
         let player = prompt("Rock, Paper or Scissors?", "Rock");
+        if (player===null){cancelCheck=true;break;}
         player = player.toLowerCase();
         if (player !== "rock" && player !== "paper" && player !== "scissors"){alert("Please enter rock, paper or scissors");i--;continue;}
         if (computerChoice.toLowerCase() === player){console.log("A Tie.");continue;}
@@ -36,6 +38,7 @@ function game()
         else if (computerChoice === "Scissors" && player === "rock"){console.log("Player win.");playerWin++;}
         else{console.log("Computer win.");computerWin++;}
     }
+    if(cancelCheck){alert("Match Cancelled");return;}
     if(computerWin==playerWin){return "Tie!";}
     else if (computerWin > playerWin){return "Computer is the final winner!";}
     else{return "Player is the final winner!";}
