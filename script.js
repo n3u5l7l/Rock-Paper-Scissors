f138d934b4a327385df2408f66d33d8a13bb6541
MingMing <greeteaz567@gmail.com>
2022-01-04 23:24:29 -0600

    Finished the function that loops the game 5 times

--- script.js
+++ script.js
@@ -13,8 +13,6 @@ function computerPlay()
 function play(computer, player)
 {
     player = player.toLowerCase();
-    console.log(player);
-    console.log(computer);
     if (computer.toLowerCase() === player){return "Tie!";}
     else if (computer === "Rock" && player === "paper"){return "Player Wins!";}
     else if (computer === "Paper" && player === "scissors"){return "Player Wins!";}
@@ -22,6 +20,25 @@ function play(computer, player)
     else{return "Computer Wins!";}
 }
 
+function game(player)
+{
+    let playerWin = 0;
+    let computerWin = 0;
+    player = player.toLowerCase();
+    for (let i = 0; i < 5; i++)
+    {
+        let computerChoice = computerPlay();
+        if (computerChoice.toLowerCase() === player){continue;}
+        else if (computerChoice === "Rock" && player === "paper"){playerWin++;}
+        else if (computerChoice === "Paper" && player === "scissors"){playerWin++;}
+        else if (computerChoice === "Scissors" && player === "rock"){playerWin++;}
+        else{computerWin++;}
+    }
+    if(computerWin==playerWin){return "Tie!";}
+    else if (computerWin > playerWin){return "Computer Wins!";}
+    else{return "Player Wins!";}
+}
+
 const playerSelection = "rock";
 const computerSelection = computerPlay();
-console.log(play(computerSelection, playerSelection));
+console.log(game(playerSelection));
